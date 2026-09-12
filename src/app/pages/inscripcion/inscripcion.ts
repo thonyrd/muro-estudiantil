7df94ecf8dc3c6e7d131cf7be70967bbd1cf53e4
Anthony Rebolledo Perez <anthony.rebolledo@virginiogomez.cl>
2026-09-11 21:31:57 -0300

    Formulario reactivo de inscripción, reglas y mensajes de validación.

--- src/app/pages/inscripcion/inscripcion.ts
+++ src/app/pages/inscripcion/inscripcion.ts
@@ -1,56 +1,97 @@
 import { Component } from '@angular/core';
+
 import {
-  ReactiveFormsModule,
-  FormGroup,
-  FormControl,
-  Validators
+
+ReactiveFormsModule,
+
+FormGroup,
+
+FormControl,
+
+Validators
+
 } from '@angular/forms';
 
 @Component({
-  selector: 'app-inscripcion',
-  imports: [ReactiveFormsModule],
-  templateUrl: './inscripcion.html',
-  styleUrl: './inscripcion.css'
+
+selector: 'app-inscripcion',
+
+imports: [ReactiveFormsModule],
+
+templateUrl: './inscripcion.html',
+
+styleUrl: './inscripcion.css'
+
 })
+
 export class Inscripcion {
 
-  formularioPedido = new FormGroup({
+formularioPedido = new FormGroup({
 
-    nombre: new FormControl('', [
-      Validators.required,
-      Validators.minLength(3)
-    ]),
+nombre: new FormControl('', [
 
-    correo: new FormControl('', [
-      Validators.required,
-      Validators.email
-    ]),
+Validators.required,
 
-    producto: new FormControl('', [
-      Validators.required
-    ]),
+Validators.minLength(3)
 
-    cantidad: new FormControl(1, [
-      Validators.required,
-      Validators.min(1)
-    ]),
+]),
 
-    direccion: new FormControl('', [
-      Validators.required,
-      Validators.minLength(5)
-    ]),
+apellido: new FormControl('', [
 
-    comentario: new FormControl('')
-  });
+Validators.required,
 
-  enviarPedido() {
-    if (this.formularioPedido.valid) {
-      console.log('Pedido enviado:', this.formularioPedido.value);
-      alert('Pedido registrado correctamente');
-      this.formularioPedido.reset({ cantidad: 1 });
-    } else {
-      alert('Debe completar correctamente los campos obligatorios');
-      this.formularioPedido.markAllAsTouched();
-    }
-  }
+Validators.minLength(3)
+
+]),
+
+correo: new FormControl('', [
+
+Validators.required,
+
+Validators.email
+
+]),
+
+carrera: new FormControl('', [
+
+Validators.required
+
+]),
+
+actividad: new FormControl('', [
+
+Validators.required
+
+]),
+
+jornada: new FormControl('', [
+
+Validators.required
+
+]),
+
+comentario: new FormControl('')
+
+});
+
+enviarPedido() {
+
+if (this.formularioPedido.valid) {
+
+console.log('Inscripción enviada:', this.formularioPedido.value);
+
+alert('Inscripción registrada correctamente');
+
+this.formularioPedido.reset();
+
+} else {
+
+alert('Debe completar correctamente los campos obligatorios');
+
+this.formularioPedido.markAllAsTouched();
+
+}
+
+}
+
 }
